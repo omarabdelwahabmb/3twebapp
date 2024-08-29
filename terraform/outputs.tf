858c74bc0d30951f97de01a9a5abc90ac07cbfa5
omarabdelwahabmb <omarabdelwahabmb@gmail.com>
2024-08-29 18:16:30 +0300

    removed output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
-output "web_dnsname" {
-  value       = module.web.aws_lb.web-load-balancer.dns_name
-}
+# output "web_dnsname" {
+#   value       = module.web.aws_lb.web-load-balancer.dns_name
+# }
